fd5fbdb6e57f7ae31823a5299a83b17d15004094
Bảo Nguyên <tn2842003@gmail.com>
2023-03-03 12:00:00 +0700

    QLNH

---
+++ database/seeders/KhachHangSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KhachHangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('khach_hangs')->delete();
+
+        DB::table('khach_hangs')->truncate();
+
+        DB::table('khach_hangs')->insert([
+            [
+                'email' => 'tn2842003@gmail.com',
+                'ho_va_ten' => 'Bảo Nguyên',
+                'password' => bcrypt(123456),
+            ],
+            [
+                'email' => 'quoclongdng@gmail.com',
+                'ho_va_ten' => 'Quốc Long',
+                'password' => bcrypt(123456),
+            ],
+            [
+                'email' => 'thanhtruong@gmail.com',
+                'ho_va_ten' => 'Thanh Trường',
+                'password' => bcrypt(123456),
+            ],
+            [
+                'email' => 'huy@gmail.com',
+                'ho_va_ten' => 'huy',
+                'password' => bcrypt(123456),
+            ],
+        ]);
+    }
+}
